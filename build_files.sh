e67f97052d55644271be4e7fc4cd81ac6b120eed
Oseni03 <oseniayomide57@gmail.com>
2024-07-07 16:14:14 +0100

    fix: still deployment

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python12 manage.py collectstatic --noinput
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.urls import re_path
+from django.conf.urls.static import static
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from drf_yasg.generators import OpenAPISchemaGenerator
@@ -30,4 +32,4 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r"^doc/", schema_view.with_ui("swagger")),
     re_path(r"^redoc/", schema_view.with_ui("redoc")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
